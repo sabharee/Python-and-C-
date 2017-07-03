8da3c861fe795548416011349dde87152f520f91
sabharee <sabariraj159@gmail.com>
2017-07-03 23:38:06 +0530

    programs

---
+++ c#/Even_or_odd.cs
@@ -0,0 +1,17 @@
+using System;      
+public class IfExample
+{
+    public static void (string[] args)
+    {
+        Console.WriteLine("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("It is even number");
+        }
+        else
+        {
+            Console.WriteLine("It is odd number");
+        }
+    }
+}  
---
+++ python/even_or_odd.py
@@ -0,0 +1,6 @@
+number=int(raw_input("enter the number"))
+a= number%2
+if a==0:
+  print "given number is even"
+else:
+  print "given number is odd" 
