5adccd8bda721a54c53f52bf24d9c4cd280fc0ea
sabharee <sabariraj159@gmail.com>
2017-07-11 21:54:34 +0530

    programs
    factorial program in C# and Python

--- python/factorial_in_python.py
+++ python/factorial_in_python.py
@@ -1,9 +1,9 @@
+n=int(raw_input("enter the number to factorial of:"))
 def factorial(n):
-  if n!=1:
-    z = n*factorial(n-1)
-    return z
-  else :
-    return 1  
-
-a=factorial(5)
-print a
+ if n!=1:
+  z=n*factorial(n-1)
+  return z
+ else:
+  return 1
+s=factorial(n)
+print s
